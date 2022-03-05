8953b24277cc295e51ebe47b8ca6a3820111a715
Andrey <andrey@localhost.localdomain>
2022-03-05 22:21:13 +0300

    Change format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
 
-int main ()
+int
+main()
 {
-    std::string name;
-    std::cout << "Enter name:";
-    std::cin >> name; //Ввод
-    std::cout << "Hello world from " << name << std::endl;//Вывод
-    return 0;
+  std::string name;
+  std::cout << "Enter name:";
+  std::cin >> name;                                      //Ввод
+  std::cout << "Hello world from " << name << std::endl; //Вывод
+  return 0;
 }
