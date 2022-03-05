5fa2d203067800170f877c8131599d95cf33f874
Andrey <andrey@localhost.localdomain>
2022-03-05 23:14:22 +0300

    solve conflict

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,18 +4,9 @@
 int
 main()
 {
-<<<<<<< HEAD
-  std::string name;
-  std::cout << "Enter name:";
-  std::cin >> name;                                      //Ввод
-  std::cout << "Hello world from " << name << std::endl; //Вывод
-  return 0;
-}
-=======
     std::string name;
     std::cout << "Enter name:";
     std::cin >> name; //input
     std::cout << "Hello world from " << name << std::endl;//output
     return 0;
 }
->>>>>>> 0974fcf (Update hello_world.cpp)
